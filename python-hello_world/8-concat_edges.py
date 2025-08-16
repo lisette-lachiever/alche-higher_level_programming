361f01435063be1d4c516160907731ebdb79bcc1
lisette-lachiever <l.mukiza@alustudent.com>
2025-08-16 04:35:48 -0700

    Update

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# Main code
 print(str[39:66] +" "+ str[107:111]+" " + str[:6])
+# Using index and concatination to form a sentence from the string str
