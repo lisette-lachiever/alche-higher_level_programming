691cf2d58f20d05faf9ee292616ca3ad53ec5f8c
Lisette <l.mukiza@alustudent.com>
2025-08-19 11:13:38 -0700

    Add white space

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for number in range(90):
     if number//10 < number % 10:
         print("{}{}".format(number//10, number % 10), end="")
-        if number!= 89:
+        if number != 89:
             print(", ", end="")
 print("")
