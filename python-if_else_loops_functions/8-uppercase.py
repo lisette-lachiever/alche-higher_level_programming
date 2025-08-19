226ce35b79c489423ecb5a6c4d3cb891abd0edb1
lisette-lachiever <l.mukiza@alustudent.com>
2025-08-19 12:41:16 -0700

    Update 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,8 @@
 def uppercase(str):
     for letter in str:
         if ord(letter) not in range(65, 91):
-            letter = (chr(ord(letter) - 32) if ord(letter) != 32 and ord(letter) != 44 else letter)
+            if ord(letter)!= 32 and ord(letter)!= 44:
+                letter = chr(ord(letter) - 32)
             print("{}".format(letter), end="")
         elif ord(letter) in range(65, 91):
             print("{}".format(letter), end="")
