0d56d8d7c186b9581a772a1748a93ec267b1f9c6
Lisette <l.mukiza@alustudent.com>
2025-08-22 14:45:58 -0700

    Update 2-args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
         if (len(argv) - 1) == 1:
             print("{} argument:".format((len(argv) - 1)))
         else:
-            print("{} arguments".format((len(argv)-1)))
+            print("{} arguments:".format((len(argv)-1)))
         for i in range(len(argv) - 1):
             print("{}:".format(i+1), argv[i+1])
