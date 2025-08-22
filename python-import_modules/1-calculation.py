c20d6a7d8964ffb51fbad57945ed8a7ffbc45de5
lisette-lachiever <l.mukiza@alustudent.com>
2025-08-22 09:12:22 -0700

    Update 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,13 +3,8 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div 
     a = 10
     b = 5
-    operation = input("Enter operation(*,/,-,+): ")
 
-    if operation == "+":
-        print("{} {} {} = {}" .format(a, operation, b, add(a, b)))
-    elif operation == "-":
-        print("{} {} {} = {}" .format(a, operation, b, sub(a, b)))
-    elif operation == "*":
-        print("{} {} {} = {}" .format(a, operation, b, mul(a, b)))
-    elif operation == "/":
-        print("{} {} {} = {}" .format(a, operation, b, div(a, b)))
+    print("{} {} {} = {}" .format(a, "+", b, add(a, b)))
+    print("{} {} {} = {}" .format(a, "-", b, sub(a, b)))
+    print("{} {} {} = {}" .format(a, "*", b, mul(a, b)))
+    print("{} {} {} = {}" .format(a, "/", b, div(a, b)))
