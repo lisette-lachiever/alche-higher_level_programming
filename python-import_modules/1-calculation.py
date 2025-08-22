e38c955a356b961ce026b78699cd831c65a5c300
lisette-lachiever <l.mukiza@alustudent.com>
2025-08-22 08:58:00 -0700

    Add 1-calculation.py

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    from calculator_1 import add sub mul div 
+    a = 10
+    b = 5
+    operation = input("Enter operation(*,/,-,+): ")
+
+    if operation == "+":
+        print("{} {} {} = {}" .format(a, operation, b, add(a, b)))
+    elif operation == "-":
+        print("{} {} {} = {}" .format(a, operation, b, sub(a, b)))
+    elif operation == "*":
+        print("{} {} {} = {}" .format(a, operation, b, mul(a, b)))
+    elif operation == "/":
+        print("{} {} {} = {}" .format(a, operation, b, div(a, b)))
+    else:
+        print("Invalid operator!")
