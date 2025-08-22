91c6311b9a3d89f84d4201bade26588ff5063b46
Lisette <l.mukiza@alustudent.com>
2025-08-22 14:44:16 -0700

    Removed trailingwhite space

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     if len(argv) == 0:
         print("0 arguments.")
     else:
-        if (len(argv) -1)== 1:
-            print("{} argument".format((len(argv) - 1)))
+        if (len(argv) - 1) == 1:
+            print("{} argument:".format((len(argv) - 1)))
         else:
             print("{} arguments".format((len(argv)-1)))
         for i in range(len(argv) - 1):
