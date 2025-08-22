29c469919f9a5ec29650d36b9096db74e29b58dd
lisette-lachiever <l.mukiza@alustudent.com>
2025-08-22 08:23:35 -0700

    Added comma

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def add(a, b):
+def add_0(a, b):
     return a+b
 
 a=1
 b=2
 if __name__ == "__main__":
-    print("{} + {} = " .format(a, b), add(a, b))
+    print("{} + {} = " .format(a, b), add_0(a, b))
