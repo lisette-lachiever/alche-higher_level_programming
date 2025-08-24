2d8f363a3fb3c05bc8e8ea5cf5c83e1a95c1b0a5
lisette-lachiever <l.mukiza@alustudent.com>
2025-08-23 19:59:13 -0700

    Update  5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,5 +4,4 @@ def no_c(my_string):
     for char in new_string:
         if char == "c" or char == "C":
             new_string.remove(char)
-    print("".join(new_string))
     return "".join(new_string)
