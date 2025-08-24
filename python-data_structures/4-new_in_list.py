3a15b27c4c6f66701f72c94fcca20fd1c93f3986
lisette-lachiever <l.mukiza@alustudent.com>
2025-08-23 18:54:43 -0700

    Upload 4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,6 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         new_list = my_list.copy()
-        print(new_list)
         return new_list
     else:
         new_list = my_list.copy()
