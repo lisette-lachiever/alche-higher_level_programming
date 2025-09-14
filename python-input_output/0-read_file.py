092460dc8e4a4c05ed25180dc8f97cd799b417f8
Lisette <l.mukiza@alustudent.com>
2025-09-14 03:07:10 -0700

    Update 0-read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 """    Function to read the given file"""
 
-
 def read_file(filename=""):
-    """Using with statement to open and read file in writing and reading mode"""
-    with open(filename, '+w') as f:
-        for line in f:
-            print(line)
-
-
+    """Using with statement to open in reading mode"""
+    with open(filename, 'r') as f:
+            print(f.read())
