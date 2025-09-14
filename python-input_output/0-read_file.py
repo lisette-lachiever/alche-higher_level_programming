b7481030c93942983c2368dd8593164392732e1a
Lisette <l.mukiza@alustudent.com>
2025-09-14 03:12:31 -0700

    Update 0-read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """    Function to read the given file"""
 
+
 def read_file(filename=""):
     """Using with statement to open in reading mode"""
     with open(filename, 'r') as f:
